34bd53e6bf35797ca64a42fc33719c38647792d8
hoangdat123 <dathv2004@gmail.com>
2024-09-26 19:28:30 +0700

    first commit

---
+++ src/main/java/org/pbl4/pbl4_be/Pbl4BeApplication.java
@@ -0,0 +1,13 @@
+package org.pbl4.pbl4_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pbl4BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pbl4BeApplication.class, args);
+    }
+
+}
